6cb1f9872d0eea836f9c4bf5245e0565a1c7d263
csamga <gabin.mascioli@gmail.com>
2023-08-19 12:39:33 +0200

    removed superflous code and added window callbacks

--- include/pyrex/pyrex.h
+++ include/pyrex/pyrex.h
@@ -1,51 +1,79 @@
 #ifndef PYREX_H_INCLUDED
 #define PYREX_H_INCLUDED
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#define PRX_FALSE 	0
+#define PRX_TRUE 	1
 
-#if !defined (_WIN32) && (defined (__WIN32__) || defined (WIN32) || defined (MINGW32))
-#	define _WIN32
-#endif // _WIN32
+#define PRX_UNSPECIFIED -1
 
-#if defined (_WIN32) && defined (PRX_BUILD_DLL)
-#	define PRXAPI __declspec(dllexport)
-#elif defined (_WIN32) && defined (PRX_USE_DLL)
-#	define PRXAPI __declspec(dllimport)
-#else
-#	define PRXAPI
-#endif // PRXAPI
+#define PRX_POS_X 		0x00001000
+#define PRX_POS_Y 		0x00001001
+#define PRX_MAXIMISED 	0x00001002
+#define PRX_RESIZABLE 	0x00001003
+#define PRX_VISIBLE   	0x00001004
+#define PRX_FOCUSED   	0x00001005
+#define PRX_DECORATED 	0x00001006
+#define PRX_DPI_SCALED 	0x00001007
 
-// PYREX MACRO CONSTANTS
-//
-#define PRX_TRUE 1
-#define PRX_FALSE 0
-
-// PYREX TYPES
-//
 typedef struct PrxWindow PrxWindow;
 
-#include <stdbool.h>
+typedef void (*PrxWindowDestroyedCb)(PrxWindow *);
+typedef void (*PrxWindowMovedCb)(PrxWindow *, int, int);
+typedef void (*PrxWindowResizedCb)(PrxWindow *, int, int);
+typedef void (*PrxWindowClosedCb)(PrxWindow *);
+typedef void (*PrxMouseMovedCb)(PrxWindow *, int, int);
 
-// PYREX API
-//
-PRXAPI PrxWindow *prx_window_create(unsigned int width,
-									unsigned int height,
-									const char *title);
+int prx_initialize(void);
+void prx_terminate(void);
+void prx_process_events(void);
 
-PRXAPI void prx_window_destroy(PrxWindow **window);
+PrxWindow *prx_window_create(int width, int height, const char *title);
+void prx_window_destroy(PrxWindow **window);
 
-PRXAPI void prx_window_set_user_data(PrxWindow *window, void *data);
-PRXAPI void *prx_window_get_user_data(PrxWindow *window);
+void prx_window_configure(int setting, int value);
 
-PRXAPI bool prx_window_had_close_request(PrxWindow *window);
+void prx_window_get_size(PrxWindow *, int *width, int *height);
+void prx_window_get_client_size(PrxWindow *, int *width, int *height);
 
-PRXAPI void prx_process_events(void);
+typedef struct HWND__ *HWND;
+HWND prx_window_get_win32_handle(PrxWindow *window);
 
-#ifdef __cplusplus
-}
-#endif
+void prx_window_set_user_pointer(PrxWindow *window, void *data);
+void *prx_window_get_user_pointer(PrxWindow *window);
+
+void prx_window_set_title(PrxWindow *window, const char *title);
+char *prx_window_get_title(PrxWindow *window);
+
+void prx_window_set_size_limits(
+	PrxWindow *window,
+	int min_width, int min_height,
+	int max_width, int max_height
+);
+
+int prx_window_is_closed(PrxWindow *window);
+
+void prx_window_set_window_destroyed_callback(
+	PrxWindow *window,
+	PrxWindowDestroyedCb callback
+);
+void prx_window_set_window_moved_callback(
+	PrxWindow *window,
+	PrxWindowMovedCb callback
+);
+void prx_window_set_window_resized_callback(
+	PrxWindow *window,
+	PrxWindowResizedCb callback
+);
+void prx_window_set_window_closed_callback(
+	PrxWindow *window,
+	PrxWindowClosedCb callback
+);
+void prx_window_set_mouse_moved_callback(
+	PrxWindow *window,
+	PrxMouseMovedCb callback
+);
+
+double prx_get_time(void);
 
 #endif // PYREX_H_INCLUDED
 
